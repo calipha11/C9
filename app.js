817711c961958a474dd50b0e0d1c84d95c8beea7
Bob <chatem112@gmail.com>
2017-05-18 15:40:03 +0000

    update dburl

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes         = require("./routes/index");
 
 
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://Cali:password@ds143231.mlab.com:43231/yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://Cali:password@ds143231.mlab.com:43231/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
